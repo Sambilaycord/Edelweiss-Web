a67f0b27ded233fc27ab6bbeb3e5aa03abe24a7b
Sambilaycord <bacalso.krystal707@gmail.com>
2026-01-25 01:07:19 +0800

    added elements in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import LoginPage from "./components/login/login_form";
+import HomePage from "./components/home/home";
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <HomePage />
     </div>
   );
 }
