d0ac4dc6601818cb2b45886586f9d856a58db39b
Sambilaycord <bacalso.krystal707@gmail.com>
2026-02-15 20:27:36 +0800

    routed the login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "./components/login/LoginPage";
 import HomePage from "./components/home/HomePage";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
