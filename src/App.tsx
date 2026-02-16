c8fd96c269e0f4e7b946bd2a899e17e44d1d8966
Sambilaycord <bacalso.krystal707@gmail.com>
2026-02-16 15:11:23 +0800

    add router to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Add this
 import LoginPage from "./components/login/LoginPage";
 import HomePage from "./components/home/home";
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Define your Login route */}
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* Define your Home route */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Optional: Redirect any unknown path to login */}
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
