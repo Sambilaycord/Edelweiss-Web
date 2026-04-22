7ef823c9377af2fa46fb514cd2da5654498c75a1
Sambilaycord <bacalso.krystal707@gmail.com>
2026-04-23 03:39:03 +0800

    deleted the sql file lol

--- src/enable_realtime.sql
+++
@@ -1,7 +0,0 @@
--- Enable Realtime for cart_items table
--- This allows the Navbar to receive "INSERT/UPDATE/DELETE" events automatically
-ALTER PUBLICATION supabase_realtime ADD TABLE cart_items;
-
--- If you get an error that it already exists, ignore it.
--- If the publication doesn't exist at all, you might need to create it:
--- CREATE PUBLICATION supabase_realtime FOR TABLE cart_items;
